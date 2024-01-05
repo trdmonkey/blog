1d50d8fe054abc62987384369c6af441d898739a
trdmonkey <trdmonkey218@gmail.com>
2024-01-05 11:08:26 -0500

    agregar detalles a la pagina de inicio

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -36,6 +36,9 @@ include('authentication.php');
   <!-- CDN DATATABLES CSS -->
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" />
 
+  <!-- CDN FONTAWESOME CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 
 <body class="g-sidenav-show  bg-gray-100">
--- admin/index.php
+++ admin/index.php
@@ -6,44 +6,73 @@
         <?= alertMessage(); ?>
     </div>
 
-    <div class="col-md-3 mb-4">
+    <div class="card-header">
+        <h4>
+            Gestión General
+        </h4>
+    </div>
+    <div class="col-md-3 mb-4 text-center">
         <div class="card card-body p-3">
             <p class="text-sm mb-0 text-capitalize font-weight-bold">Usuarios Totales</p>
             <h5 class="font-weight-bolder mb-0">
                 <?= getCount('users'); ?>
             </h5>
+            <!-- <i class="fas fa-users fa-3x mt-3"></i> --> <!-- Icono de usuarios -->
+            <i class="fa fa-street-view fa-3x mt-3" aria-hidden="true"></i>
         </div>
     </div>
     <div class="col-md-3 mb-4">
-        <div class="card card-body p-3">
+        <div class="card card-body p-3 text-center">
             <p class="text-sm mb-0 text-capitalize font-weight-bold">Servicios Totales</p>
             <h5 class="font-weight-bolder mb-0">
                 <?= getCount('services'); ?>
             </h5>
+            <i class="fa fa-shopping-bag fa-3x mt-3" aria-hidden="true"></i>
         </div>
     </div>
     <div class="col-md-3 mb-4">
-        <div class="card card-body p-3">
+        <div class="card card-body p-3 text-center">
+            <p class="text-sm mb-0 text-capitalize font-weight-bold">Servicios Ocultos</p>
+            <h5 class="font-weight-bolder mb-0">
+                <?php
+                $query = "SELECT * FROM services WHERE status='1'";
+                $result = mysqli_query($conn, $query);
+                $totalCount = mysqli_num_rows($result);
+                echo $totalCount;
+                ?>
+            </h5>
+            <i class="fa fa-eye-slash fa-3x mt-3" aria-hidden="true"></i>
+        </div>
+    </div>
+    <div class="col-md-3 mb-4">
+        <div class="card card-body p-3 text-center">
             <p class="text-sm mb-0 text-capitalize font-weight-bold">Redes Sociales Totales</p>
             <h5 class="font-weight-bolder mb-0">
                 <?= getCount('social_medias'); ?>
             </h5>
+            <i class="fa fa-thumbs-o-up fa-3x mt-3" aria-hidden="true"></i>
         </div>
     </div>
 </div>
 
 <div class="row">
+    <div class="card-header">
+        <h4>
+            Gestión De Consultas
+        </h4>
+    </div>
     <div class="col-md-3 mb-4">
-        <div class="card card-body p-3">
+        <div class="card card-body p-3 text-center">
             <p class="text-sm mb-0 text-capitalize font-weight-bold">Consultas Totales</p>
             <h5 class="font-weight-bolder mb-0">
                 <?= getCount('enquires'); ?>
             </h5>
+            <i class="fa fa-calendar fa-3x mt-3" aria-hidden="true"></i>
         </div>
     </div>
 
     <div class="col-md-3 mb-4">
-        <div class="card card-body p-3">
+        <div class="card card-body p-3 text-center">
             <p class="text-sm mb-0 text-capitalize font-weight-bold">Consultas Hoy</p>
             <h5 class="font-weight-bolder mb-0">
                 <?php
@@ -55,6 +84,7 @@
                 echo $totalCount;
                 ?>
             </h5>
+            <i class="fa fa-calendar-plus-o fa-3x mt-3" aria-hidden="true"></i>
         </div>
     </div>
 
@@ -71,10 +101,11 @@
                 echo $totalCount;
                 ?>
             </h5>
+            <i class="fa fa-calendar-check-o fa-3x mt-3" aria-hidden="true"></i>
         </div>
     </div>
 
-    <div class="col-md-3 mb-4">
+    <div class="col-md-3 mb-3">
         <div class="card card-body p-3 text-center">
             <p class="text-sm mb-0 text-capitalize font-weight-bold">Consultas Canceladas</p>
             <h5 class="font-weight-bolder mb-0">
@@ -87,6 +118,7 @@
                 echo $totalCount;
                 ?>
             </h5>
+            <i class="fa fa-calendar-times-o fa-3x mt-3" aria-hidden="true"></i>
         </div>
     </div>
 </div>

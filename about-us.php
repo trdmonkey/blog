69ffc444c1bf53c6cf92f264ae317a984e95ba47
trdmonkey <trdmonkey218@gmail.com>
2024-01-05 18:59:36 -0500

    añadir estilos y diseño al modulo ACERCA DE MI

--- about-us.php
+++ about-us.php
@@ -5,7 +5,26 @@ include('includes/header.php');
 
 ?>
 
+<div class="py-5 bg-secondary">
+    <div class="container">
+        <h4 class="text-white text-center">Acerca de mi</h4>
+    </div>
+</div>
 
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4>Acerca de <?= webSetting('title'); ?></h4>
+                <div class="underline"></div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta earum fugiat ullam quidem molestiae iure possimus rem iusto? Laborum at eos architecto quos. Porro fugiat ullam natus ipsa, ducimus laborum.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta earum fugiat ullam quidem molestiae iure possimus rem iusto? Laborum at eos architecto quos. Porro fugiat ullam natus ipsa, ducimus laborum.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
--- index.php
+++ index.php
@@ -53,6 +53,68 @@ include('includes/header.php');
     </div>
 </div>
 
+<div class="py-5 bg-light">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-12 mb-4 text-center">
+                <h4>Nuestros Servicios</h4>
+                <div class="underline mx-auto"></div>
+            </div>
+
+            <?php
+            $serviceQuery = "SELECT * FROM services WHERE status='0' LIMIT 6";
+            $result = mysqli_query($conn, $serviceQuery);
+
+            if($result) {
+
+                if(mysqli_num_rows($result) > 0) {
+
+                    foreach($result as $row) {
+                        ?>
+                        <div class="col-md-4 mb-3">
+                            <div class="card shadow">
+
+                                <?php if($row['image'] != "") : ?>
+                                    <img src="<?= $row['image']; ?>" class="w-100 rounded" alt="Image" style="min-height: 200px; max-height: 200px;" />
+                                <?php else: ?>
+                                    <img src="assets/images/tim.png" class="w-100 rounded" alt="Image" style="min-height: 200px; max-height: 200px;" />
+                                <?php endif; ?>
+
+                                <div class="card-body">
+                                    <h5><?= $row['name']; ?></h5>
+                                    <p>
+                                        <?= $row['small_description']; ?>
+                                    </p>
+                                    <div class="">
+                                        <a href="service.php?slug=<?= $row['slug']; ?>" target="_blank" class="text-primary">Leer mas...</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <?php
+                    }
+
+                } else {
+                    ?>
+                    <div class="col-md-12">
+                        <h5>Ningun registro encontrado.</h5>
+                    </div>
+                    <?php
+                }
+
+            } else {
+                ?>
+                <div class="col-md-12">
+                    <h5>Algo Salió Mal!</h5>
+                </div>
+                <?php
+            }
+            ?>
+        </div>
+    </div>
+</div>
+
 
 
 <?php include('includes/footer.php'); ?>

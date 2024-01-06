503d6bf2791044a6f06bffd2837e4650fed01a1b
trdmonkey <trdmonkey218@gmail.com>
2024-01-06 08:55:55 -0500

    agregar detalles al front

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,3 +1,9 @@
+<?php
+$pageName = substr($_SERVER['SCRIPT_NAME'], strrpos($_SERVER['SCRIPT_NAME'],"/") +1);
+// $pageName = basename($_SERVER['SCRIPT_NAME']); /* HACE LO MISMO PERO MAS OPTIMIZADO Y PRACTICO */
+// echo $pageName;
+?>
+
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
@@ -11,10 +17,10 @@
 
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link  active" href="index.php">
+                <a class="nav-link <?= $pageName == 'index.php' ? 'active' : ''; ?>" href="index.php">
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <!-- <i class="fa fa-home text-dark text-lg"></i> -->
-                        <i class="fa fa-tachometer text-dark text-lg" aria-hidden="true"></i>
+                        <i class="fa fa-tachometer <?= $pageName == 'index.php' ? 'text-white' : 'text-dark'; ?> text-lg" aria-hidden="true"></i>
                     </div>
                     <span class="nav-link-text ms-1">Inicio</span>
                 </a>
@@ -24,10 +30,10 @@
                 <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Consultas</h6>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="enquiries.php">
+                <a class="nav-link <?= $pageName == 'enquiries.php' ? 'active' : ''; ?>" href="enquiries.php">
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <!-- <i class="fa fa-bullhorn text-white text-lg"></i> -->
-                        <i class="fa fa-coffee text-dark text-lg" aria-hidden="true"></i>
+                        <i class="fa fa-coffee <?= $pageName == 'enquiries.php' ? 'text-white' : 'text-dark'; ?> text-lg" aria-hidden="true"></i>
 
                     </div>
                     <span class="nav-link-text ms-1">Consultas</span>
@@ -38,9 +44,9 @@
                 <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Gestionar Servicios</h6>
             </li>
             <li class="nav-item">
-                <a class="nav-link  " href="services.php">
+                <a class="nav-link <?= $pageName == 'services.php' ? 'active' : ''; ?>" href="services.php">
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="fa fa-tty text-dark text-lg" aria-hidden="true"></i>
+                        <i class="fa fa-tty <?= $pageName == 'services.php' ? 'text-white' : 'text-dark'; ?> text-lg" aria-hidden="true"></i>
                     </div>
                     <span class="nav-link-text ms-1">Servicios</span>
                 </a>
@@ -50,27 +56,27 @@
                 <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Administrador</h6>
             </li>
             <li class="nav-item">
-                <a class="nav-link  " href="users.php">
+                <a class="nav-link <?= $pageName == 'users.php' ? 'active' : ''; ?>" href="users.php">
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="fa fa-user-circle-o text-dark text-lg" aria-hidden="true"></i>
+                        <i class="fa fa-user-circle-o <?= $pageName == 'users.php' ? 'text-white' : 'text-dark'; ?> text-lg" aria-hidden="true"></i>
                     </div>
                     <span class="nav-link-text ms-1">Admin | Usuarios</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link  " href="social-media.php">
+                <a class="nav-link <?= $pageName == 'social-media.php' ? 'active' : ''; ?>" href="social-media.php">
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <!-- <i class="fa fa-cogs text-white text-lg"></i> -->
-                        <i class="fa fa-superpowers text-dark text-lg" aria-hidden="true"></i>
+                        <i class="fa fa-superpowers <?= $pageName == 'social-media.php' ? 'text-white' : 'text-dark'; ?> text-lg" aria-hidden="true"></i>
                     </div>
                     <span class="nav-link-text ms-1">Medios</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link  " href="settings.php">
+                <a class="nav-link <?= $pageName == 'settings.php' ? 'active' : ''; ?>" href="settings.php">
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <!-- <i class="fa fa-cogs text-white text-lg"></i> -->
-                        <i class="fa fa-cogs text-dark text-lg" aria-hidden="true"></i>
+                        <i class="fa fa-cogs <?= $pageName == 'settings.php' ? 'text-white' : 'text-dark'; ?> text-lg" aria-hidden="true"></i>
                     </div>
                     <span class="nav-link-text ms-1">Ajustes</span>
                 </a>
